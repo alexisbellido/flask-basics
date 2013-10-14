19a19631b9938b1fd8eae006c419ad4c614487d9
Alexis Bellido <alexis@ventanazul.com>
2013-10-14 16:32:47 -0400

    try redirect, abort and cookies

--- hello.py
+++ hello.py
@@ -1,24 +1,38 @@
 from flask import Flask, url_for
 from flask import render_template
 from flask import request
+from flask import make_response
+from flask import abort, redirect
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def  index():
-    return 'Index page. Method: %s' % request.method
+    resp = make_response('some text for index')
+    resp.set_cookie('color', 'red')
+    return resp
+
+    #return 'Index page. Method: %s' % request.method
 
 @app.route('/hello/', methods=['GET', 'POST'])
 @app.route('/hello/<name>', methods=['GET', 'POST'])
 def  hello(name='Anybody'):
     print "request method", request.method
-    print "key", request.args.get('key', '')
+    key = request.args.get('key', '')
+    print "key", key
+    print "cookies", request.cookies
     if request.method == 'POST':
         #print "username", request.form['username']
         name = request.form.get('username', 'no username')
         print "username", name
     #app.logger.debug('This is the logger line for %s' % name)
+    if key == 'redirect':
+        return redirect(url_for('index'))
+    if key == '404':
+        abort(404)
+    if key == '401':
+        abort(401)
     return render_template('hello.html', name=name)
 
 @app.route('/user/<username>')
